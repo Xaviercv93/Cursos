1764b927a80e4907bb38892b2a873cde9ab226f3
xaviercv93 <xaviercv93@gmail.com>
2020-06-15 02:20:49 -0500

    2.8 Ejercicio Concatenacion String y Contexto String
    Concatenar 2 strings
    Aplicar contexto string entre numeros y String

--- Java/001FundamentosJava/Leccion01/HolaMundo/src/main/java/HolaMundo.java
+++ Java/001FundamentosJava/Leccion01/HolaMundo/src/main/java/HolaMundo.java
@@ -12,20 +12,36 @@ public class HolaMundo {
 
     public static void main(String lalo[]) {
 
-        System.out.println("Hola Mundo");
+//        System.out.println("Hola Mundo");
+//
+//        String saludar = "Saludos";
+//
+//        System.out.println(saludar);
+//        System.out.println(saludar);
+//        System.out.println(saludar);
+//
+//        //var infiere el tipo de dato 
+//        var despedirse = "Hasta luego";
+//        System.out.println(despedirse);
+//        
+//        var numero = 1;
+//        System.out.println(numero);
+//Concatenacion de cadenas
+//String ES EL UNICO OPERADOR SOBRECARGADO
+//String es el unico operador con 2 funciones, concatenar y sumar
+        var usuario = "Juan";
+        var saludar = "Hola";
 
-        String saludar = "Saludos";
+        System.out.println(usuario + " " + saludar);
 
-        System.out.println(saludar);
-        System.out.println(saludar);
-        System.out.println(saludar);
+//Ejemplos de Contexto String
+//Las expresiones se evaluan de izquierda a derecha
+        var i = 1;
+        var j = 2;
 
-        //var infiere el tipo de dato 
-        var despedirse = "Hasta luego";
-        System.out.println(despedirse);
-        
-        var numero = 1;
-        System.out.println(numero);
+        System.out.println(usuario + i + j);
+
+        System.out.println(i + j + usuario);
 
     }
 }
