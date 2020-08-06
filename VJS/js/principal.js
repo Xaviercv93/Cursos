25b3b0bb00bee6daa570f66ace7ccf9a1f3e4b2d
xaviercv93 <xaviercv93@gmail.com>
2020-08-06 13:59:49 -0500

    ejemplo de metodos y directiva v-on
    v-on en el boton guardar
    se creo el metodo agregarNotas

--- VJS/js/principal.js
+++ VJS/js/principal.js
@@ -7,11 +7,31 @@ var app = new Vue({
         //mensaje : 'Hola vue.js!'
         //arreglo
         lista: [
-            { nombre: 'Juan Carlos', promedio: '10', estado: false },
-            { nombre: 'Liliana Nataly', promedio: '17', estado: true },
-            { nombre: 'Luis Alberto', promedio: '18', estado: true }
+            // { nombre: 'Juan Carlos', promedio: '10', estado: false },
+            // { nombre: 'Liliana Nataly', promedio: '17', estado: true },
+            // { nombre: 'Luis Alberto', promedio: '18', estado: true }
 
         ],
-    }
+        nombre: '',
+        promedio: ''
+    },
+
+    //metodos
+    methods: {
+        agregarNota: function () {
+            estadoA = false;
+            if (this.promedio >= 12.5) {
+                estadoA = true;
+            }
+            if (this.nombre != "" && this.promedio != "") {
+                this.lista.push({ nombre: this.nombre, promedio: this.promedio, estado: estadoA });
+                this.nombre="";
+                this.promedio="";
+        
+            } else {
+                alert("Ingrese el nombre y promedio del estudiante");
+            }
+        }
+    },
 
 })
