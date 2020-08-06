027f7d1e47a3c6969c3a72335f25543b65c7ad31
xaviercv93 <xaviercv93@gmail.com>
2020-08-06 13:45:43 -0500

    ejemplo de tabla y directivas for - bind
    v-for
    v-bind
    se cambio en principal.js mensaje por table y se añadio en index.html las directivas que trabajen con los datos de table

--- VJS/js/principal.js
+++ VJS/js/principal.js
@@ -2,9 +2,16 @@
 var app = new Vue({
 
     //vue puede controlar al objeto con etiqueta app
-    el : '#app',
-    data : {
-        mensaje : 'Hola vue.js!'
+    el: '#app',
+    data: {
+        //mensaje : 'Hola vue.js!'
+        //arreglo
+        lista: [
+            { nombre: 'Juan Carlos', promedio: '10', estado: false },
+            { nombre: 'Liliana Nataly', promedio: '17', estado: true },
+            { nombre: 'Luis Alberto', promedio: '18', estado: true }
+
+        ],
     }
 
 })
