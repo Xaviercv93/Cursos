e747f3f7a5129e84faceb89aaeaf2b63d1edb88e
xaviercv93 <xaviercv93@gmail.com>
2020-08-06 13:26:52 -0500

    Ejemplo de propiedades de vue js
    el
    data

---
+++ VJS/js/principal.js
@@ -0,0 +1,10 @@
+//instancia de objeto vuejs
+var app = new Vue({
+
+    //vue puede controlar al objeto con etiqueta app
+    el : '#app',
+    data : {
+        mensaje : 'Hola vue.js!'
+    }
+
+})
